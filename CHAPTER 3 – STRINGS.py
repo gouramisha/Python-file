02f94114d2b51ae468c4d3583688be13d2d42704
gouramisha <amishagour6@gmail.com>
2025-05-19 23:41:14 +0530

    First Commit

---
+++ CHAPTER 3 – STRINGS.py
@@ -0,0 +1,24 @@
+# String is a data type in python.
+# String is a sequence of characters enclosed in quotes.
+# We can primarily write a string in these three ways.
+# Kaam	Python Method
+# Sabko chhota banaye	           lower()
+# Sabko bada banaye	               upper()
+# Sirf pehla letter bada	       capitalize()
+# Har word ka pehla letter bada  	title()
+
+                       # SLICING WITH SKIP VALUE
+# len () function – This function returns the length of the strings.
+str = "Amisha"
+print(len(str)) # Output: 5
+# String.endswith("sha")
+str = "Amisha"
+print(str.endswith("sha")) # Output: True
+# string.count("") – counts the total number of occurrences of any character.
+str = "amisha"
+count = str.count("a")
+print(count) # Output: 2
+# 4. the first character of a given string.
+str = "amishalok"
+capitalized_string = str.capitalize()
+print(capitalized_string) # Output: Amishalok
