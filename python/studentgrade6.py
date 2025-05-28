507b82a36809637f5d1fc1af26bc2ae4c2b6ec96
gouramisha <amishagour6@gmail.com>
2025-05-28 21:46:45 +0530

    loo

---
+++ python/studentgrade6.py
@@ -0,0 +1,16 @@
+marks = int(input("Enter the number"))
+if 90<= marks <=100:
+    print("Ex")
+elif 80<= marks <=90:
+    print("A")
+elif 70<= marks <=80:
+    print("B") 
+elif 60<= marks <=70:
+    print("c")
+elif 50<= marks <=60:
+    print("D")
+elif 0<= marks <=50:
+    print("E") 
+
+else:
+     "Invalid marks"
