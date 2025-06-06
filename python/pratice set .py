122ef6566f49dab2fd8a6b0e4a2e4e4772bf1d0d
gouramisha <amishagour6@gmail.com>
2025-06-06 21:43:57 +0530

    PS - 1

--- python/pratice set .py
+++ python/pratice set .py
@@ -4,3 +4,16 @@ if num % 5 == 0:
     print("Divisible by 5")
 else: 
     print("Not Divisible by 5")
+
+
+
+#Write a program to calculate area of rectangle (length × breadth).
+length = float(input("Enter length: "))
+breadth = float(input("Enter breadth: "))
+
+area = length * breadth
+print("The area of rectangle is:", area)
+#Write a program that takes your birth year and shows your age.
+
+
+
