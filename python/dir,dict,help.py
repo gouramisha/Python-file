00d2ae5807871b3c3201fbf6b177337eb894a3fb
gouramisha <amishagour6@gmail.com>
2025-06-10 23:42:55 +0530

    magic dunder

--- python/dir,dict,help.py
+++ python/dir,dict,help.py
@@ -1,3 +1,4 @@
-x = [1, 2, 3]
-print(dir(x))
-print(x.__add__)
+# Concept	Meaning
+# dir(obj)	Lists all available attributes and methods
+# __add__	Defines what + does for the object
+# obj.__add__(other)	Called when you do obj + other
