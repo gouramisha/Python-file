f11c0f0392e6311b4a0f22c07c56ad027a5f0776
gouramisha <amishagour6@gmail.com>
2025-06-21 00:52:15 +0530

    request module

--- python/Practice Set/pratice set 2.py
+++ python/Practice Set/pratice set 2.py
@@ -6,3 +6,9 @@ while i <= n:
     sum += i
     i += 1
 print("sum of first", n, "natural numbers is:", sum)
+
+#Star Pattern: *, ***, ***** for n = 3
+n = 3
+for i in range(n):
+    stars = '*' * (2*i + 1)
+    print(stars)
