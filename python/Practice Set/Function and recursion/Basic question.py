c53706ebf2ad8c8c5ee15bbb591c974c43917730
gouramisha <amishagour6@gmail.com>
2025-06-25 00:54:26 +0530

    Function and recursion

--- python/Practice Set/Function and recursion/Basic question.py
+++ python/Practice Set/Function and recursion/Basic question.py
@@ -1,4 +1,7 @@
 #  Add Two Numbers Using a Function
+from numpy import negative
+
+
 def add(a, b):
     return a + b
 
@@ -10,3 +13,30 @@ def square(n):
     return n * n
 
 print(square(4))  
+
+#Write a function to find the maximum of three numbers.
+def max_of_three(a, b, c):
+    return max(a, b,c)
+print (max_of_three (10, 20 , 30))
+
+#Write a function to check whether a number is positive, negative or zero.
+def check_number(n):
+    if n > 0:
+        return "Positive"
+    elif n < 0:
+        return "Negative"
+    else:
+        return "Zero"
+
+print(check_number(-5))  # Output: Negative
+
+# Write a function to calculate the factorial of a number using recursion.
+
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+print(factorial(5))  # Output: 120
+
+
