1ba944b2cc62c2df4c49d64814a0a7803018866f
gouramisha <amishagour6@gmail.com>
2025-06-25 00:06:03 +0530

    function recursion

--- python/Practice Set/Function and recursion/Basic question.py
+++ python/Practice Set/Function and recursion/Basic question.py
@@ -3,3 +3,10 @@ def add(a, b):
     return a + b
 
 print("Sum:", add(3, 4))  # Output: 7
+
+#Write a function to return the square of a number.
+
+def square(n):
+    return n * n
+
+print(square(4))  
