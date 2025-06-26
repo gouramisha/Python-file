aa92b06508bc6c5083c6f51c0faec54c0328a9fa
gouramisha <amishagour6@gmail.com>
2025-06-26 11:09:42 +0530

    Function pratice

--- python/Practice Set/Function and recursion/Basic question.py
+++ python/Practice Set/Function and recursion/Basic question.py
@@ -39,4 +39,14 @@ def factorial(n):
 
 print(factorial(5))  # Output: 120
 
+# Write a function to calculate the factorial of a number using recursion.
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+print(factorial(5))  # Output: 120
+
+
+
 
