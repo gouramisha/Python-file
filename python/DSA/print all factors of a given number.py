e0dadebf5235962b81053882a27bb7a304c69c47
gouramisha <amishagour6@gmail.com>
2025-07-08 17:52:06 +0530

    print all factors

--- python/DSA/print all factors of a given number.py
+++ python/DSA/print all factors of a given number.py
@@ -7,3 +7,18 @@ while i <= n:
     if n % i == 0:
         print(i)
     i += 1
+
+
+# optimization code
+# import math
+
+# n = int(input("Enter a number: "))
+# print("Factors of", n, "are:")
+
+# i = 1
+# while i <= math.isqrt(n):  # isqrt(n) gives integer square root (√n)
+#     if n % i == 0:
+#         print(i)
+#         if i != n // i:  # Avoid duplicate when i == sqrt(n)
+#             print(n // i)
+#     i += 1
