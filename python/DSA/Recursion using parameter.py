0145a5da3213774f154a330952e3c390d7f7321c
gouramisha <amishagour6@gmail.com>
2025-07-09 23:35:32 +0530

    Recusion using parameter

--- python/DSA/Recursion using parameter.py
+++ python/DSA/Recursion using parameter.py
@@ -1,4 +1,14 @@
-def func(x, N):
-    print(x)
-    function(x,n)
-    func(15, 4)
+# def func(x, N):
+#     print(x)
+#     function(x,n)
+#     func(15, 4)
+
+
+def print_numbers(i, n):
+    if i > n:      # Jab i n se bada ho jaaye to ruk jao
+        return
+    print(i)       # Number print karo
+    print_numbers(i + 1, n)   # Next number ke liye call karo
+
+# Function call karo
+print_numbers(1, 5)
