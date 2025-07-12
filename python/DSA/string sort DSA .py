3789e53052eef8d526dc8f92e588ff49df92451f
gouramisha <amishagour6@gmail.com>
2025-07-12 23:41:08 +0530

    sort string

---
+++ python/DSA/string sort DSA .py
@@ -0,0 +1,14 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        # Swap the found min with the first element
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+# Example usage
+arr = [64, 25, 12, 22, 11]
+selection_sort(arr)
+print("Sorted array:", arr)
