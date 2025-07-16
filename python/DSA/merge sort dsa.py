db57a5a4d144d784956d2ff506c165845ebed54b
gouramisha <amishagour6@gmail.com>
2025-07-16 23:49:44 +0530

    merge sort arr

---
+++ python/DSA/merge sort dsa.py
@@ -0,0 +1,12 @@
+def merge_array (left , right):
+    result = [ ]
+    i , j = 0 , 0
+    n, num = len(left), len (right)
+    while i <n and j <m:
+        if left i <= right[j]
+           result.append(left[i])
+        i+=1 
+    else:
+     result.append (right[j])
+     j+=1.
+
