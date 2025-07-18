6c28b619de2f9c839807ff05dbd6e8188383151b
gouramisha <amishagour6@gmail.com>
2025-07-18 23:42:28 +0530

    second largest number element in an array

---
+++ python/DSA/Array/Array dsa 2.py
@@ -0,0 +1,13 @@
+def find_second_largest(arr):
+    first = second = float('-inf')
+
+    for num in arr:
+        if num > first:
+            second = first
+            first = num
+        elif num > second and num != first:
+            second = num
+
+    return second
+
+print(find_second_largest([12, 35, 1, 10, 34, 1]))
