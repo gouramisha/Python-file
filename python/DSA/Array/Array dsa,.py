8a711e182ca2750d0d2cf3fa3fe65f739e1a6c74
gouramisha <amishagour6@gmail.com>
2025-07-18 23:30:22 +0530

    Largest array number

--- python/DSA/Array/Array dsa,.py
+++ python/DSA/Array/Array dsa,.py
@@ -1,7 +1,20 @@
-nums = [11, 20 , 330 , -9 ]
-largest = nums[0]
-n = len (nums)
-for i in range (0, n):
-    largest max(largest , nums[i])
+# nums = [11, 20 , 330 , -9 ]
+# largest = nums[0]
+# n = len (nums)
+# for i in range (0, n):
+#     largest max(largest , nums[i])
+
+arr = [4, 9, 1, 17, 3]
+
+# Step 1: Assume first number is biggest
+largest = arr[0]
+
+# Step 2: Check every number
+for num in arr:
+    if num > largest:
+        largest = num
+
+# Step 3: Print result
+print("Largest number is:", largest)
  
    
