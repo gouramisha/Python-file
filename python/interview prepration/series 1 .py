5152abe8853f01a26c74efdac2577cd598dd2ac4
gouramisha <amishagour6@gmail.com>
2025-07-20 06:03:40 +0530

    starting basics interview question

---
+++ python/interview prepration/series 1 .py
@@ -0,0 +1,12 @@
+# Data Structures & Algorithms with Python
+# ✅ Basic to Advanced Topics:
+# Topic	Key Python Tools
+# Arrays/Lists	list, slicing, append(), pop()
+# Strings	split(), join(), slicing, in
+# HashMaps & Sets	dict, set, collections.Counter
+# Stacks/Queues	list, collections.deque()
+# Linked List	Custom class, OOP implementation
+# Trees/Graphs	Classes, recursion, DFS/BFS
+# Sorting/Searching	sorted(), binary search, merge sort
+# Recursion	Base + Recursive case 2.
+# DP & Backtracking	Memoization with @lru_cache
