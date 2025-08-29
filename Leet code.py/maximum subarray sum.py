378417d62b0769281d667c892d39b1519d982002
gouramisha <amishagour6@gmail.com>
2025-08-29 23:08:16 +0530

    maximum subarray sum

---
+++ Leet code.py/maximum subarray sum.py
@@ -0,0 +1,10 @@
+total = 0
+maximum = 0
+maximum = float ("-inf")
+n = len(nums)
+maximum = float("-inf")
+total = 0
+for i in range (0, n):
+    total = total + nums [j]
+    maximum  = max(maximum, total)
+
