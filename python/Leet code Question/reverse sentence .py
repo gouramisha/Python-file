d25165a809b158d3eea8a99b988390b1d11dd007
gouramisha <amishagour6@gmail.com>
2025-09-01 23:50:49 +0530

    reverese sentence

---
+++ python/Leet code Question/reverse sentence .py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def reverseWords(self, s):
+        
+        
+        words = s.split()
+        
+       
+        words.reverse()
+        
+        
+        return " ".join(words)
